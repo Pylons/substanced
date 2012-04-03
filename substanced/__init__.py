2e1fa84597517cf003da648705d82e561262a6e8
Chris McDonough <chrism@plope.com>
2012-04-03 02:19:48 -0400

    moved here

--- substanced/__init__.py
+++ substanced/__init__.py
@@ -3,6 +3,7 @@ def includeme(config):
     config.include('substanced.content')
     config.include('substanced.catalog')
     config.include('substanced.models')
+    config.include('substanced.subscribers')
     config.include('substanced.sdi')
     
     
