7ad195a86f410edb052f1e78e1c4483c73e62cd4
Chris McDonough <chrism@plope.com>
2012-04-02 23:15:49 -0400

    fix root factory

--- substanced/site.py
+++ substanced/site.py
@@ -1,4 +1,6 @@
 import transaction
+import colander
+
 from zope.interface import implementer
 
 from pyramid_zodbconn import get_connection
@@ -6,25 +8,29 @@ from pyramid_zodbconn import get_connection
 from .interfaces import IDocmapSite
 from .docmap import DocumentMap
 from .folder import Folder
+from .sdi import Schema
+
+class SiteSchema(Schema):
+    name = colander.SchemaNode(colander.String())
+    description = colander.SchemaNode(colander.String())
 
 @implementer(IDocmapSite)
 class Site(Folder):
+    name = ''
+    description = ''
+    __schema__ = SiteSchema()
     def __init__(self):
         Folder.__init__(self)
         self.docmap = DocumentMap()
     
     @classmethod
-    def root_factory(cls, *arg, **kw):
-        def root_factory(
-                request,
-                transaction=transaction,
-                get_connection=get_connection,
-                ): # transaction and get_connection for testing
-            conn = get_connection(request)
-            zodb_root = conn.root()
-            if not 'app_root' in zodb_root:
-                app_root = cls(*arg, **kw)
-                zodb_root['app_root'] = app_root
-                transaction.commit()
-            return zodb_root['app_root']
-        return root_factory
+    def root_factory(cls, request):
+        # this is a classmethod so that it works when Site is subclassed.
+        conn = get_connection(request)
+        zodb_root = conn.root()
+        if not 'app_root' in zodb_root:
+            app_root = cls()
+            zodb_root['app_root'] = app_root
+            transaction.commit()
+        return zodb_root['app_root']
+
