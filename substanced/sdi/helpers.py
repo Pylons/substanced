f7293f36601030c7cb0f988a2b6131aadfa1d1fd
Chris McDonough <chrism@plope.com>
2012-04-05 06:00:54 -0400

    add security, modify master a bit

--- substanced/sdi/helpers.py
+++ substanced/sdi/helpers.py
@@ -3,6 +3,10 @@ from zope.interface import Interface
 
 from pyramid.traversal import find_resource
 from pyramid.renderers import get_renderer
+from pyramid.security import (
+    view_execution_permitted,
+    has_permission,
+    )
 
 from substanced.objectmap import find_objectmap
 
@@ -13,10 +17,13 @@ def get_subnodes(request, context=None):
         context = request.context
     objectmap = find_objectmap(context)
     nodes = objectmap.navgen(context, depth=1)
+    L = []
     for node in nodes:
         obj = find_resource(context, node['path'])
-        node['url'] = request.mgmt_path(obj)
-    return nodes
+        if has_permission('view', obj, request):
+            node['url'] = request.mgmt_path(obj)
+            L.append(node)
+    return L
 
 def get_mgmt_views(request, context=None):
     registry = request.registry
@@ -33,7 +40,8 @@ def get_mgmt_views(request, context=None):
                 if iface.providedBy(context):
                     view_name = discriminator[2]
                     if view_name:
-                        L.append(view_name)
+                        if view_execution_permitted(context, request,view_name):
+                            L.append(view_name)
     return L
 
 def macros():
