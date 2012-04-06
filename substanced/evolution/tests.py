5ca90280752f154bf0e6d1003866ac1e958314f1
Chris McDonough <chrism@plope.com>
2012-04-06 04:49:10 -0400

    really add evolution tests

---
+++ substanced/evolution/tests.py
@@ -0,0 +1,22 @@
+import unittest
+from pyramid import testing
+
+class Test_add_evolution_package(unittest.TestCase):
+    def _callFUT(self, config, package_name):
+        from . import add_evolution_package
+        return add_evolution_package(config, package_name)
+
+    def test_it(self):
+        from repoze.evolution import ZODBEvolutionManager
+        from repoze.evolution import IEvolutionManager
+        config = testing.DummyResource()
+        L = []
+        def registerUtility(*arg, **kw):
+            L.append((arg, kw))
+        registry = testing.DummyResource(registerUtility=registerUtility)
+        config.registry = registry
+        self._callFUT(config, 'package')
+        self.assertEqual(L, [((ZODBEvolutionManager, IEvolutionManager), 
+                              {'name': 'kuiuecomm.evolution'})])
+        
+        
