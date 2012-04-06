63c4bfe7737586d473d81cc7b81acb0e8f177105
Chris McDonough <chrism@plope.com>
2012-04-06 04:46:38 -0400

    test add_evolution_package

--- substanced/evolution/__init__.py
+++ substanced/evolution/__init__.py
@@ -7,5 +7,5 @@ def add_evolution_package(config, package_name):
     config.registry.registerUtility(ZODBEvolutionManager, IEvolutionManager, 
                                     name='kuiuecomm.evolution')
 
-def includeme(config):
+def includeme(config): #pragma: no cover
     config.add_directive('add_evolution_package', add_evolution_package)
