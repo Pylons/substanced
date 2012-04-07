8a3a9853cde66c3bfc45bf3019c965a9313542ee
Chris McDonough <chrism@plope.com>
2012-04-06 22:04:37 -0400

    new services lookup regime

---
+++ substanced/service.py
@@ -0,0 +1,15 @@
+from pyramid.location import lineage
+
+from .interfaces import (
+    IFolder,
+    SERVICES_NAME
+    )
+
+def find_service(context, name):
+    for obj in lineage(context):
+        if IFolder.providedBy(obj):
+            services = obj.get(SERVICES_NAME)
+            if services is not None:
+                if name in services:
+                    return services[name]
+                
