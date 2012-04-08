c62ab12187126b4d3fd79f48710ccc4579c3ae11
Chris McDonough <chrism@plope.com>
2012-04-08 02:42:46 -0400

    configure a session authentication policy by default

--- demo/demo/__init__.py
+++ demo/demo/__init__.py
@@ -1,6 +1,5 @@
 from pyramid.config import Configurator
 from substanced.site import Site
-from pyramid.session import UnencryptedCookieSessionFactoryConfig
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
@@ -8,8 +7,5 @@ def main(global_config, **settings):
     config = Configurator(settings=settings, root_factory=Site.root_factory)
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.include('substanced')
-    secret = settings['substanced.secret']
-    session_factory = UnencryptedCookieSessionFactoryConfig(secret)
-    config.set_session_factory(session_factory)
     config.scan()
     return config.make_wsgi_app()
