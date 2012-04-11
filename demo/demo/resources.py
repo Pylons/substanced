f6d3902c4b146403d432d65a82324451eebd1bea
Chris McDonough <chrism@plope.com>
2012-04-11 05:47:06 -0400

    add an exists validator, fix mentions of description

--- demo/demo/resources.py
+++ demo/demo/resources.py
@@ -14,9 +14,21 @@ from substanced.content import content
 from substanced.sdi import mgmt_view
 from substanced.form import FormView
 
+@colander.deferred
+def exists_validator(node, kw):
+    context = kw['request'].context
+    if DocumentType.providedBy(context):
+        context = context.__parent__
+    def exists(node, value):
+        if value in context:
+            raise colander.Invalid(node, 'Object named "%s" already exists' % 
+                                   value)
+    return exists
+
 class DocumentSchema(Schema):
     name = colander.SchemaNode(
         colander.String(),
+        validator = exists_validator,
         )
     title = colander.SchemaNode(
         colander.String(),
@@ -31,14 +43,15 @@ class DocumentType(IPropertied):
 
 @content(DocumentType, icon='icon-file')
 class Document(Persistent):
+
     __propschema__ = DocumentSchema()
+
     def __init__(self, title, body):
         self.title = title
         self.body = body
         
     def get_properties(self):
-        return dict(name=self.__name__, description=self.description,
-                    title=self.title)
+        return dict(name=self.__name__, body=self.body, title=self.title)
 
     def set_properties(self, struct):
         newname = struct['name']
@@ -46,14 +59,14 @@ class Document(Persistent):
         if newname != oldname:
             parent = self.__parent__
             parent.rename(oldname, newname)
-        self.description = struct['description']
+        self.body = struct['body']
         self.title = struct['title']
     
 @mgmt_view(context=IFolder, name='add document', tab_title='Add Document', 
            permission='add content', 
            renderer='substanced.sdi:templates/form.pt')
 class AddUserView(FormView):
-    title = 'Add User'
+    title = 'Add Document'
     schema = DocumentSchema()
     buttons = ('add',)
 
