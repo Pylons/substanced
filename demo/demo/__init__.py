6aa98e7c316a5260dbff1188692c376d704682e0
Chris McDonough <chrism@plope.com>
2012-04-11 05:32:40 -0400

    add a document type to the demo

--- demo/demo/__init__.py
+++ demo/demo/__init__.py
@@ -5,7 +5,6 @@ def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     config = Configurator(settings=settings, root_factory=Site.root_factory)
-    config.add_static_view('static', 'static', cache_max_age=3600)
     config.include('substanced')
     config.scan()
     return config.make_wsgi_app()
---
+++ demo/demo/resources.py
@@ -0,0 +1,61 @@
+from persistent import Persistent
+
+from pyramid.httpexceptions import HTTPFound
+
+import colander
+import deform.widget
+
+from substanced.schema import Schema
+from substanced.content import content
+from substanced.content import Type
+from substanced.sdi import mgmt_view
+from substanced.form import FormView
+
+class DocumentSchema(Schema):
+    name = colander.SchemaNode(
+        colander.String(),
+        )
+    title = colander.SchemaNode(
+        colander.String(),
+        )
+    body = colander.SchemaNode(
+        colander.String(),
+        widget=deform.widget.RichTextWidget()
+        )
+
+class DocumentType(Type):
+    pass
+
+@content(DocumentType, icon='icon-file')
+class Document(Persistent):
+    __propschema__ = DocumentSchema()
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+        
+    def get_properties(self):
+        return dict(name=self.__name__, description=self.description,
+                    title=self.title)
+
+    def set_properties(self, struct):
+        newname = struct['name']
+        oldname = self.__name__
+        if newname != oldname:
+            parent = self.__parent__
+            parent.rename(oldname, newname)
+        self.description = struct['description']
+        self.title = struct['title']
+    
+@mgmt_view(context=DocumentType, name='add', tab_title='Add Document')
+class AddUserView(FormView):
+    title = 'Add User'
+    schema = DocumentSchema()
+    buttons = ('add',)
+
+    def add_success(self, appstruct):
+        registry = self.request.registry
+        name = appstruct.pop('name')
+        document = registry.content.create(DocumentType, **appstruct)
+        self.request.context[name] = document
+        return HTTPFound(self.request.mgmt_path(document, '@@properties'))
+    
