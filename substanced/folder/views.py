67fcee5991c0c00706850538cb7b4949e1ce821b
Chris McDonough <chrism@plope.com>
2012-04-13 05:38:22 -0400

    move sdi/contents.py to folder/views.py

--- substanced/folder/views.py
+++ substanced/folder/views.py
@@ -1,11 +1,20 @@
 import colander
 
 from pyramid.httpexceptions import HTTPFound
+from pyramid.security import has_permission
 
 from ..schema import Schema
 from ..form import FormView
-from ..interfaces import IFolder
-from ..sdi import mgmt_view
+from ..sdi import (
+    mgmt_view,
+    get_batchinfo,
+    check_csrf_token,
+    )
+
+from ..interfaces import (
+    IFolder,
+    SERVICES_NAME
+    )
 
 @colander.deferred
 def name_validator(node, kw):
@@ -41,4 +50,47 @@ class AddFolderView(FormView):
         folder = registry.content.create(IFolder)
         self.request.context[name] = folder
         return HTTPFound(location=self.request.mgmt_path(folder, '@@contents'))
+
+# TODO: rename, cut, copy, paste
+
+@mgmt_view(context=IFolder, name='contents', 
+           renderer='substanced.sdi:templates/contents.pt',
+           permission='view')
+def folder_contents(context, request):
+    can_manage = has_permission('manage contents', context, request)
+    L = []
+    for k, v in context.items():
+        viewable = False
+        url = request.mgmt_path(v)
+        if has_permission('view', v, request):
+            viewable = True
+        icon = request.registry.content.metadata(v, 'icon')
+        deletable = can_manage and k != SERVICES_NAME
+        data = dict(name=k, deletable=deletable, viewable=viewable, url=url, 
+                    icon=icon)
+        L.append(data)
+    batchinfo = get_batchinfo(L, request, url=request.url)
+    return dict(batchinfo=batchinfo)
+
+@mgmt_view(context=IFolder, name='delete_folder_contents',
+           permission='manage contents', tab_condition=False)
+def delete_folder_contents(context, request):
+    check_csrf_token(request)
+    todelete = request.POST.getall('delete')
+    deleted = 0
+    for name in todelete:
+        v = context.get(name)
+        if v is not None:
+            del context[name]
+            deleted += 1
+    if not deleted:
+        msg = 'No items deleted'
+        request.session.flash(msg)
+    elif deleted == 1:
+        msg = 'Deleted 1 item'
+        request.flash_undo(msg)
+    else:
+        msg = 'Deleted %s items' % deleted
+        request.flash_undo(msg)
+    return HTTPFound(request.mgmt_path(context, '@@contents'))
 
