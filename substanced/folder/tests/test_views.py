9a605dd1c21a671c8888dbbab2aab3ac18cba2c2
Chris McDonough <chrism@plope.com>
2012-04-13 01:46:45 -0400

    test name_validator

---
+++ substanced/folder/tests/test_views.py
@@ -0,0 +1,36 @@
+import unittest
+from pyramid import testing
+import colander
+
+class Test_name_validator(unittest.TestCase):
+    def _callFUT(self, node, kw):
+        from ..views import name_validator
+        return name_validator(node, kw)
+
+    def _makeKw(self, exc=None):
+        request = testing.DummyRequest()
+        request.context = DummyFolder(exc)
+        return dict(request=request)
+
+    def test_it_exception(self):
+        exc = KeyError('wrong')
+        kw = self._makeKw(exc)
+        node = object()
+        v = self._callFUT(node, kw)
+        self.assertRaises(colander.Invalid, v, node, 'abc')
+
+    def test_it_no_exception(self):
+        kw = self._makeKw()
+        node = object()
+        v = self._callFUT(node, kw)
+        result = v(node, 'abc')
+        self.assertEqual(result, None)
+         
+class DummyFolder(object):
+    def __init__(self, exc=None):
+        self.exc = exc
+
+    def check_name(self, name):
+        if self.exc:
+            raise self.exc
+        return name
