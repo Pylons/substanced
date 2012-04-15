f6a36e24509b3eef65ea2e374cb730222235db73
Chris McDonough <chrism@plope.com>
2012-04-15 02:03:28 -0400

    use request_param

--- substanced/catalog/views.py
+++ substanced/catalog/views.py
@@ -17,25 +17,25 @@ class ManageCatalog(object):
     def __init__(self, context, request):
         self.context = context
         self.request = request
+        self.redir_location=self.request.mgmt_path(
+            self.context, '@@manage_catalog')
 
     @mgmt_view(request_method='GET', tab_title='Manage')
     def GET(self):
         cataloglen = len(self.context.objectids)
         return dict(cataloglen=cataloglen)
 
-    @mgmt_view(request_method='POST')
-    def POST(self):
+    @mgmt_view(request_method='POST', request_param='reindex')
+    def reindex(self):
         check_csrf_token(self.request)
-        location=self.request.mgmt_path(self.context, '@@manage_catalog')
-        if 'reindex' in self.request.POST:
-            self.context.reindex()
-            self.request.session.flash('Catalog reindexed')
-            return HTTPFound(location=location)
-        elif 'refresh' in self.request.POST:
-            self.context.refresh(registry=self.request.registry)
-            self.request.session.flash('Catalog refreshed')
-            return HTTPFound(location=location)
-        else:
-            self.request.session.flash('Unknown command', 'error')
-            return HTTPFound(location=location)
+        self.context.reindex()
+        self.request.session.flash('Catalog reindexed')
+        return HTTPFound(location=self.redir_location)
+
+    @mgmt_view(request_method='POST', request_param='refresh')
+    def refresh(self):
+        check_csrf_token(self.request)
+        self.context.refresh(registry=self.request.registry)
+        self.request.session.flash('Catalog refreshed')
+        return HTTPFound(location=self.redir_location)
 
