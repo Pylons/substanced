d437d92a29992db8391843e2d162800c43d44d3b
Chris McDonough <chrism@plope.com>
2012-04-16 06:44:46 -0400

    add a catalog search form

--- substanced/catalog/views.py
+++ substanced/catalog/views.py
@@ -1,9 +1,18 @@
+import colander
+import deform_bootstrap.widget
+
 from pyramid.httpexceptions import HTTPFound
 
 from pyramid.view import view_defaults
 
 from ..interfaces import ICatalog
 from ..sdi import mgmt_view
+from ..form import FormView
+from ..schema import Schema
+from ..service import find_service
+from ..util import oid_of
+
+from . import logger
 
 @view_defaults(
     name='manage_catalog',
@@ -34,3 +43,91 @@ class ManageCatalog(object):
         self.request.session.flash('Catalog refreshed')
         return HTTPFound(location=self.redir_location)
 
+@colander.deferred
+def principals_widget(node, kw):
+    request = kw['request']
+    principals = find_service(request.context, 'principals')
+    groups = [(str(oid_of(group)), name) for name, group in 
+              principals['groups'].items()]
+    users = [(str(oid_of(user)), name) for name, user in 
+              principals['users'].items()]
+    values = (
+        {'label':'Groups', 'values':groups}, 
+        {'label':'Users', 'values':users},
+        )
+    widget = deform_bootstrap.widget.ChosenOptGroupWidget(values=values)
+    return widget
+
+class Principals(colander.SequenceSchema):
+    principal = colander.SchemaNode(
+        colander.Int(),
+        missing=colander.null,
+        widget = principals_widget,
+        )
+
+class Permitted(Schema):
+    permission = colander.SchemaNode(
+        colander.String(),
+        missing='',
+        )
+    principals = Principals(missing=colander.null)
+
+class SearchSchema(Schema):
+    cqe_expression = colander.SchemaNode(
+        colander.String(),
+        title='CQE Expression',
+        )
+    permitted = Permitted(title='Principals and Permission Filter')
+
+@mgmt_view(context=ICatalog, name='search_catalog', 
+           permission='sdi.manage-catalog', 
+           renderer='templates/searchform.pt', tab_title='Search')
+class SearchCatalogView(FormView):
+    title = 'Search Catalog'
+    schema = SearchSchema(title='Expression')
+    buttons = ('search',)
+    catalog_results = None
+
+    def search_success(self, appstruct):
+        """ Accept a CQE expression and a permitted value and return a 
+        sequence of object renderings """
+        self.request.session['catalogsearch.appstruct'] = appstruct
+        context = self.request.context
+        return HTTPFound(
+            location=self.request.mgmt_path(context, '@@search_catalog')
+            )
+
+    def show(self, form):
+        try:
+            appstruct = self.request.session.pop('catalogsearch.appstruct',
+                                                 colander.null)
+        except KeyError:
+            appstruct = colander.null # work around pyramid_redis_sessions bug
+        searchresults = ()
+        if appstruct:
+            permitted = appstruct['permitted']
+            permission = permitted['permission']
+            principals = permitted['principals']
+            if not permission:
+                permitted = None
+            else:
+                permitted = principals, permission
+            expr = appstruct['cqe_expression']
+            try:
+                n, oids, res = self.request.query_catalog(
+                    expr, permitted=permitted)
+            except Exception as e:
+                logger.exception('During search')
+                cls_name = e.__class__.__name__
+                msg = 'Could not parse query.. %s: %s' % (cls_name, e.args[0])
+                self.request.session.flash(msg, 'error')
+            else:
+                searchresults = list([(x, res(x)) for x in oids])
+                if not searchresults:
+                    searchresults = [('', 'No results')]
+                self.request.session.flash('Query succeeded', 'success')
+        return {
+            'searchresults':searchresults,
+            'form':form.render(appstruct=appstruct),
+            }
+        
