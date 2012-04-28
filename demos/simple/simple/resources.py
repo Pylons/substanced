b58e272cd69a9fbeff8db70953a7de4b9a1c7bc4
Chris McDonough <chrism@plope.com>
2012-04-27 20:08:55 -0400

    split upload from basic props in demo to show example of properties with multiple subtabs

--- demos/simple/simple/resources.py
+++ demos/simple/simple/resources.py
@@ -1,4 +1,3 @@
-import mimetypes
 import StringIO
 import colander
 import deform.widget
@@ -6,21 +5,13 @@ import deform.widget
 from persistent import Persistent
 from ZODB.blob import Blob
 
-from pyramid.httpexceptions import HTTPFound
-from pyramid.response import FileResponse
-
 from substanced.interfaces import (
-    IFolder,
     IPropertied,
     ICatalogable,
     )
 from substanced.schema import Schema
 from substanced.content import content
-from substanced.sdi import mgmt_view
-from substanced.form import (
-    FormView,
-    FileUploadTempStore,
-    )
+from substanced.form import FileUploadTempStore
 from substanced.util import chunks
 
 def make_name_validator(content_type):
@@ -86,8 +77,9 @@ class DocumentPropertySheet(object):
     name='Document',
     )
 class Document(Persistent):
-
-    __propsheets__ = ( ('', DocumentSchema()), )
+    __propsheets__ = (
+        ('', DocumentPropertySheet),
+        )
 
     def __init__(self, title, body):
         self.title = title
@@ -96,26 +88,6 @@ class Document(Persistent):
     def texts(self): # for indexing
         return self.title, self.body
         
-@mgmt_view(
-    context=IFolder,
-    name='add_document',
-    tab_title='Add Document', 
-    permission='sdi.add-content', 
-    renderer='substanced.sdi:templates/form.pt',
-    tab_condition=False,
-    )
-class AddDocumentView(FormView):
-    title = 'Add Document'
-    schema = DocumentSchema()
-    buttons = ('add',)
-
-    def add_success(self, appstruct):
-        registry = self.request.registry
-        name = appstruct.pop('name')
-        document = registry.content.create(DocumentType, **appstruct)
-        self.request.context[name] = document
-        return HTTPFound(self.request.mgmt_path(document, '@@properties'))
-
 class FileType(IPropertied, ICatalogable):
     pass
     
@@ -125,12 +97,7 @@ def upload_widget(node, kw):
     tmpstore = FileUploadTempStore(request)
     return deform.widget.FileUploadWidget(tmpstore)
 
-class FileSchema(Schema):
-    file = colander.SchemaNode(
-        deform.schema.FileData(),
-        widget = upload_widget,
-        missing = colander.null,
-        )
+class FilePropertiesSchema(Schema):
     name = colander.SchemaNode(
         colander.String(),
         validator = make_name_validator(FileType),
@@ -141,24 +108,42 @@ class FileSchema(Schema):
         missing = colander.null,
         )
 
-@colander.deferred
-def name_or_file(node, kw):
-    def _name_or_file(node, struct):
-        if not struct['file'] and not struct['name']:
-            raise colander.Invalid('One of name or file is required')
-        if not struct['name']:
-            if struct['file'] and struct['file'].get('filename'):
-                filename = struct['file']['filename']
-                curried_name_validator = make_name_validator(FileType)
-                real_name_validator = curried_name_validator(node, kw)
-                real_name_validator(node['file'], filename)
-    return _name_or_file
+class FileUploadSchema(Schema):
+    file = colander.SchemaNode(
+        deform.schema.FileData(),
+        widget = upload_widget,
+        missing = colander.null,
+        )
 
 class FilePropertySheet(object):
     def __init__(self, context, request):
         self.context = context
         self.request = request
-        self.schema = FileSchema(validator=name_or_file).bind(request=request)
+        self.schema = FilePropertiesSchema().bind(request=request)
+
+    def get(self):
+        context = self.context
+        return dict(
+            name=context.__name__,
+            mimetype=context.mimetype
+            )
+
+    def set(self, struct):
+        context = self.context
+        newname = struct['name']
+        mimetype = struct['mimetype']
+        if not mimetype:
+            mimetype = 'application/octet-stream'
+        context.mimetype = mimetype
+        oldname = context.__name__
+        if newname and newname != oldname:
+            context.__parent__.rename(oldname, newname)
+
+class FileUploadPropertySheet(object):
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+        self.schema = FileUploadSchema().bind(request=request)
 
     def get(self):
         context = self.context
@@ -167,31 +152,15 @@ class FilePropertySheet(object):
             uid=str(context.__objectid__),
             filename=context.__name__,
             )
-        return dict(
-            name=context.__name__,
-            file=filedata,
-            mimetype=self.mimetype
-            )
-
+        return dict(file=filedata)
+    
     def set(self, struct):
         context = self.context
-        newname = struct['name']
         file = struct['file']
-        mimetype = struct['mimetype']
         if file and file.get('fp'):
             fp = file['fp']
             fp.seek(0)
             context.upload(fp)
-            filename = file['filename']
-            mimetype = mimetypes.guess_type(filename, strict=False)[0]
-            if not newname:
-                newname = filename
-        if not mimetype:
-            mimetype = 'application/octet-stream'
-        context.mimetype = mimetype
-        oldname = context.__name__
-        if newname and newname != oldname:
-            context.__parent__.rename(oldname, newname)
         
 @content(
     FileType,
@@ -204,7 +173,10 @@ class File(Persistent):
     # prevent view tab from sorting first (it would display the file when
     # manage_main clicked)
     __tab_order__ = ('properties', 'acl_edit', 'view')
-    __propsheets__ = ( ('', FilePropertySheet), )
+    __propsheets__ = (
+        ('basics', FilePropertySheet),
+        ('upload', FileUploadPropertySheet),
+        )
 
     def __init__(self, stream, mimetype='application/octet-stream'):
         self.mimetype = mimetype
@@ -222,61 +194,3 @@ class File(Persistent):
         fp.close()
         self.size = size
 
-@mgmt_view(
-    context=IFolder,
-    name='add_file',
-    tab_title='Add File', 
-    permission='sdi.add-content', 
-    renderer='substanced.sdi:templates/form.pt',
-    tab_condition=False
-    )
-class AddFileView(FormView):
-    title = 'Add File'
-    schema = FileSchema(validator=name_or_file)
-    buttons = ('add',)
-
-    def add_success(self, appstruct):
-        registry = self.request.registry
-        name = appstruct.pop('name')
-        filedata = appstruct.pop('file')
-        stream = None
-        filename = None
-        if filedata:
-            filename = filedata['filename']
-            stream = filedata['fp']
-            if stream:
-                stream.seek(0)
-            else:
-                stream = None
-        mimetype = appstruct['mimetype']
-        name = filename or name
-        if not mimetype:
-            mimetype = mimetypes.guess_type(name, strict=False)[0]
-            if mimetype is None:
-                mimetype = 'application/octet-stream'
-        fileob = registry.content.create(FileType, stream, mimetype)
-        self.request.context[name] = fileob
-        return HTTPFound(self.request.mgmt_path(fileob, '@@properties'))
-
-@mgmt_view(
-    context=FileType,
-    name='view',
-    tab_title='View',
-    permission='sdi.view'
-    )
-def view_tab(context, request):
-    return HTTPFound(location=request.mgmt_path(context))
-    
-@mgmt_view(
-    context=FileType,
-    name='', 
-    permission='sdi.view',
-    tab_condition=False
-    )
-def view_file(context, request):
-    return FileResponse(
-        context.blob.committed(),
-        request=request,
-        content_type=str(context.mimetype),
-        )
-    
--- demos/simple/simple/views.py
+++ demos/simple/simple/views.py
@@ -1,8 +1,129 @@
+import mimetypes
+import colander
+import deform.schema
+
+from pyramid.httpexceptions import HTTPFound
+from pyramid.response import FileResponse
 from pyramid.view import view_config
 
+from substanced.sdi import mgmt_view
+from substanced.form import (
+    FormView
+    )
+from substanced.interfaces import (
+    IFolder
+    )
+
+from .resources import (
+    FilePropertiesSchema,
+    DocumentSchema,
+    upload_widget,
+    FileType,
+    DocumentType,
+    make_name_validator,
+    )
+
 @view_config(renderer='templates/splash.pt')
-def default_view(request):
+def splash_view(request):
     manage_prefix = request.registry.settings.get('substanced.manage_prefix', 
                                                   '/manage')
     return {'manage_prefix': manage_prefix}
+
+@colander.deferred
+def name_or_file(node, kw):
+    def _name_or_file(node, struct):
+        if not struct['file'] and not struct['name']:
+            raise colander.Invalid('One of name or file is required')
+        if not struct['name']:
+            if struct['file'] and struct['file'].get('filename'):
+                filename = struct['file']['filename']
+                curried_name_validator = make_name_validator(FileType)
+                real_name_validator = curried_name_validator(node, kw)
+                real_name_validator(node['file'], filename)
+    return _name_or_file
+
+class AddFileSchema(FilePropertiesSchema):
+    file = colander.SchemaNode(
+        deform.schema.FileData(),
+        widget = upload_widget,
+        missing = colander.null,
+        )
+
+@mgmt_view(
+    context=IFolder,
+    name='add_file',
+    tab_title='Add File', 
+    permission='sdi.add-content', 
+    renderer='substanced.sdi:templates/form.pt',
+    tab_condition=False
+    )
+class AddFileView(FormView):
+    title = 'Add File'
+    schema = AddFileSchema(validator=name_or_file)
+    buttons = ('add',)
+
+    def add_success(self, appstruct):
+        registry = self.request.registry
+        name = appstruct.pop('name')
+        filedata = appstruct.pop('file')
+        stream = None
+        filename = None
+        if filedata:
+            filename = filedata['filename']
+            stream = filedata['fp']
+            if stream:
+                stream.seek(0)
+            else:
+                stream = None
+        mimetype = appstruct['mimetype']
+        name = filename or name
+        if not mimetype:
+            mimetype = mimetypes.guess_type(name, strict=False)[0]
+            if mimetype is None:
+                mimetype = 'application/octet-stream'
+        fileob = registry.content.create(FileType, stream, mimetype)
+        self.request.context[name] = fileob
+        return HTTPFound(self.request.mgmt_path(fileob, '@@properties'))
+
+@mgmt_view(
+    context=FileType,
+    name='view',
+    tab_title='View',
+    permission='sdi.view'
+    )
+def view_tab(context, request):
+    return HTTPFound(location=request.mgmt_path(context))
+    
+@mgmt_view(
+    context=FileType,
+    name='', 
+    permission='sdi.view',
+    tab_condition=False
+    )
+def view_file(context, request):
+    return FileResponse(
+        context.blob.committed(),
+        request=request,
+        content_type=str(context.mimetype),
+        )
+    
+@mgmt_view(
+    context=IFolder,
+    name='add_document',
+    tab_title='Add Document', 
+    permission='sdi.add-content', 
+    renderer='substanced.sdi:templates/form.pt',
+    tab_condition=False,
+    )
+class AddDocumentView(FormView):
+    title = 'Add Document'
+    schema = DocumentSchema()
+    buttons = ('add',)
+
+    def add_success(self, appstruct):
+        registry = self.request.registry
+        name = appstruct.pop('name')
+        document = registry.content.create(DocumentType, **appstruct)
+        self.request.context[name] = document
+        return HTTPFound(self.request.mgmt_path(document, '@@properties'))
 
