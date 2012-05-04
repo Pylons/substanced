5d0eb9d5944c8d999e28300c5bd1c71095244917
Paul Everitt <paul@agendaless.com>
2012-05-04 15:27:05 -0400

    Made a step02 that didn't have SDI link

---
+++ docs/tutorial/tutorial/step02/index.rst
@@ -0,0 +1,5 @@
+
+Shows
+======
+
+- Substance D bootstrapping, minimum
--- docs/tutorial/tutorial/step03/views.py
+++ docs/tutorial/tutorial/step03/views.py
@@ -2,4 +2,6 @@ from pyramid.view import view_config
 
 @view_config(renderer="templates/hello.pt")
 def hello_view(request):
-    return {"tutorial": "Little Dummy"}
+    manage_prefix = request.registry.settings.get(
+        'substanced.manage_prefix', '/manage')
+    return {'manage_prefix': manage_prefix}
