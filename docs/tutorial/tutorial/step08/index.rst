37b895a04bb9c55759012c06c659729684df2cef
Paul Everitt <paul@agendaless.com>
2012-05-08 10:19:13 -0400

    Prep for step08

---
+++ docs/tutorial/tutorial/step08/index.rst
@@ -0,0 +1,13 @@
+
+Shows
+=====
+
+- List documents in retail view
+
+- Split views into mgmt and retail
+
+- Introduce interfaces
+
+- Main site view is registered against SiteEntry
+
+- View registered for IDocument
---
+++ docs/tutorial/tutorial/step08/mgmt.py
@@ -0,0 +1,32 @@
+from pyramid.httpexceptions import HTTPFound
+
+from substanced.sdi import mgmt_view
+from substanced.form import FormView
+from substanced.interfaces import (
+    IFolder,
+    )
+
+from .resources import (
+    DocumentSchema,
+    )
+
+@mgmt_view(
+    context=IFolder,
+    name='add_document',
+    tab_title='Add Document',
+    permission='sdi.add-content',
+    renderer='substanced.sdi:templates/form.pt',
+    tab_condition=False,
+    )
+class AddDocumentView(FormView):
+    title = 'Add Document'
+    schema = DocumentSchema()
+    buttons = ('add',)
+
+    def add_success(self, appstruct):
+        registry = self.request.registry
+        name = appstruct.pop('name')
+        document = registry.content.create('Document', **appstruct)
+        self.context[name] = document
+        return HTTPFound(self.request.mgmt_path(document, '@@properties'))
+
