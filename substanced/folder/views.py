c650426f04c1418f5137531ed28bbfd0a445ff67
Carlos de la Guardia <cguardia@yahoo.com>
2012-10-15 00:47:06 -0500

    add datatables jquery plugin and change folder contents template to use that for sortng and searching instead of batches

--- substanced/folder/views.py
+++ substanced/folder/views.py
@@ -117,8 +117,7 @@ class FolderContentsViews(object):
             headers = [column['name'] for column in sd_columns]
         seq = self.sdi_folder_contents(context, request) # generator
         addables = self.sdi_add_views(request, context)
-        batch = Batch(seq, request, seqlen=len(context))
-        return dict(batch=batch, addables=addables, headers=headers)
+        return dict(items=seq, addables=addables, headers=headers)
 
     @mgmt_view(
         request_method='POST',
