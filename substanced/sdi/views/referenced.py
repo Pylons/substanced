56c65777dba1893ee257dd19db0ae99de714cdd4
Chris McDonough <chrism@plope.com>
2012-11-06 12:49:16 -0500

    add referenced management view

---
+++ substanced/sdi/views/referenced.py
@@ -0,0 +1,47 @@
+from pyramid.view import view_defaults
+
+from ...objectmap import find_objectmap
+from ...util import oid_of
+
+from .. import (
+    mgmt_view,
+    MIDDLE
+    )
+
+@view_defaults(
+    referenced=True,
+    name='references',
+    permission='sdi.manage-references',
+    )
+class ReferencedView(object):
+
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+    
+    @mgmt_view(
+        renderer='templates/referenced.pt',
+        tab_title='References',
+        tab_after=MIDDLE, # try not to be the default tab, we're too obscure
+        )
+    def show(self):
+        oid = oid_of(self.context)
+        objectmap = find_objectmap(self.context)
+        targets = []
+        sources = []
+        for reftype in objectmap.get_reftypes():
+            targetids = objectmap.targetids(oid, reftype)
+            if targetids:
+                targets.append((reftype, self._paths(targetids, objectmap)))
+            sourceids = objectmap.sourceids(oid, reftype)
+            if sourceids:
+                sources.append((reftype, self._paths(sourceids, objectmap)))
+        return {'targets':targets, 'sources':sources}
+
+    def _paths(self, ids, objectmap):
+        for id in ids:
+            path_tuple = objectmap.path_for(id)
+            path = '/'.join(path_tuple)
+            yield path
+        
+                
