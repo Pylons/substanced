3a9e7356dd78c76d9a0b20f75832087ff9db2d6e
Chris McDonough <chrism@plope.com>
2012-12-19 05:20:00 -0500

    cope with MultipleUndoErrors at commit time; turn undoviews into a class so i can mock out transaction

--- substanced/sdi/views/tests/test_undo.py
+++ substanced/sdi/views/tests/test_undo.py
@@ -1,42 +1,45 @@
 import unittest
 from pyramid import testing
 
-class Test_undo_one(unittest.TestCase):
-    def _callFUT(self, request):
-        from ..undo import undo_one
-        return undo_one(request)
+class TestUndoViews(unittest.TestCase):
+    def _makeOne(self, request):
+        from ..undo import UndoViews
+        return UndoViews(request)
 
-    def test_no_referrer(self):
+    def test_undo_one_no_referrer(self):
         conn = DummyConnection()
         request = testing.DummyRequest()
         request._primary_zodb_conn = conn # XXX not an API, will break
         request.sdiapi = DummySDIAPI()
         request.referrer = None
         request.params['hash'] = 'hash'
-        resp = self._callFUT(request)
+        inst = self._makeOne(request)
+        resp = inst.undo_one()
         self.assertEqual(resp.location, '/mgmt_path')
         
-    def test_with_referrer(self):
+    def test_undo_one_with_referrer(self):
         conn = DummyConnection()
         request = testing.DummyRequest()
         request._primary_zodb_conn = conn # XXX not an API, will break
         request.sdiapi = DummySDIAPI()
         request.referrer = 'loc'
         request.params['hash'] = 'hash'
-        resp = self._callFUT(request)
+        inst = self._makeOne(request)
+        resp = inst.undo_one()
         self.assertEqual(resp.location, 'loc')
 
-    def test_no_undo_info(self):
+    def test_undo_one_no_undo_info(self):
         conn = DummyConnection()
         request = testing.DummyRequest()
         request._primary_zodb_conn = conn # XXX not an API, will break
         request.sdiapi = DummySDIAPI()
         request.referrer = 'loc'
         request.params['hash'] = 'hash'
-        self._callFUT(request)
+        inst = self._makeOne(request)
+        inst.undo_one()
         self.assertEqual(request.session['_f_error'], ['Could not undo, sorry'])
         
-    def test_with_undo_info_no_match(self):
+    def test_undo_one_with_undo_info_no_match(self):
         record = {'description':'desc', 'id':'abc'}
         conn = DummyConnection(undo_info=[record])
         request = testing.DummyRequest()
@@ -44,10 +47,11 @@ class Test_undo_one(unittest.TestCase):
         request.sdiapi = DummySDIAPI()
         request.referrer = 'loc'
         request.params['hash'] = 'hash'
-        self._callFUT(request)
+        inst = self._makeOne(request)
+        inst.undo_one()
         self.assertEqual(request.session['_f_error'], ['Could not undo, sorry'])
 
-    def test_with_undo_info_match(self):
+    def test_undo_one_with_undo_info_match(self):
         record = {'description':'hash:abc', 'id':'abc'}
         conn = DummyConnection(undo_info=[record])
         request = testing.DummyRequest()
@@ -55,11 +59,15 @@ class Test_undo_one(unittest.TestCase):
         request.sdiapi = DummySDIAPI()
         request.referrer = 'loc'
         request.params['hash'] = 'abc'
-        self._callFUT(request)
+        inst = self._makeOne(request)
+        transaction = DummyTransaction()
+        inst.transaction = transaction
+        inst.undo_one()
         self.assertEqual(len(request.session['_f_success']), 1)
         self.assertEqual(len(conn._db.undone), 1)
+        self.assertEqual(transaction.committed, True)
 
-    def test_with_undo_info_POSError(self):
+    def test_undo_one_with_undo_info_POSError(self):
         from ZODB.POSException import POSError
         record = {'description':'hash:abc', 'id':'abc'}
         conn = DummyConnection(undo_info=[record], undo_exc=POSError)
@@ -68,9 +76,13 @@ class Test_undo_one(unittest.TestCase):
         request.sdiapi = DummySDIAPI()
         request.referrer = 'loc'
         request.params['hash'] = 'abc'
-        self._callFUT(request)
+        transaction = DummyTransaction()
+        inst = self._makeOne(request)
+        inst.transaction = transaction
+        inst.undo_one()
         self.assertEqual(len(request.session['_f_error']), 1)
         self.assertEqual(len(conn._db.undone), 0)
+        self.assertEqual(transaction.aborted, True)
 
 class DummyDB(object):
     def __init__(self, supports_undo, undo_info, undo_exc=None):
@@ -97,3 +109,11 @@ class DummyConnection(object):
 class DummySDIAPI(object):
     def mgmt_path(self, *arg, **kw):
         return '/mgmt_path'
+
+class DummyTransaction(object):
+    def commit(self):
+        self.committed = True
+
+    def abort(self):
+        self.aborted = True
+        
