b5aa0c8e4e8cf431afca43434359c43a9ecd47ce
Chris McDonough <chrism@plope.com>
2013-06-10 08:47:40 -0400

    print profile results regardless of system exit

--- substanced/util/__init__.py
+++ substanced/util/__init__.py
@@ -583,14 +583,16 @@ def profile(
     """Allow for profiling of the entire process (e.g. during a bulk load)"""
     fd, fn = tempfile.mkstemp()
     try:
-        _profile.runctx(cmd, globals, locals, fn)
-        stats = pstats.Stats(fn)
-        stats.strip_dirs()
-        # calls,time,cumulative and cumulative,calls,time are useful
-        stats.sort_stats(*sort_order or ('cumulative', 'calls', 'time'))
-        if callers:
-            stats.print_callers(.3)
-        else:
-            stats.print_stats(.3)
+        try:
+            _profile.runctx(cmd, globals, locals, fn)
+        finally:
+            stats = pstats.Stats(fn)
+            stats.strip_dirs()
+            # calls,time,cumulative and cumulative,calls,time are useful
+            stats.sort_stats(*sort_order or ('cumulative', 'calls', 'time'))
+            if callers:
+                stats.print_callers(.3)
+            else:
+                stats.print_stats(.3)
     finally:
         os.remove(fn)
