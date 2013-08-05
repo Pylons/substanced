7673108897aa31035fb497e92fb34d95c7f7c072
Chris McDonough <chrism@plope.com>
2013-08-05 14:41:45 -0400

    centralize loading_indicator_on logic

---
+++ substanced/sdi/static/js/sdi.js
@@ -0,0 +1,11 @@
+function sdi_loading_indicator_on() {
+    $('#sdi-loading-img').css('display', 'inline');
+    return true;
+}
+
+function sdi_loading_indicator_off() {
+    $('#sdi-loading-img').css('display', 'none');
+    return true;
+}
+
+
