bd0e9cc5850ba9009e27eddc30c33a1aa24d9c8a
Chris McDonough <chrism@plope.com>
2013-10-08 09:50:41 +0200

    unused imports

--- substanced/scaffolds/substanced/+package+/views.py
+++ substanced/scaffolds/substanced/+package+/views.py
@@ -1,12 +1,9 @@
 from pyramid.httpexceptions import HTTPFound
-from pyramid.renderers import get_renderer
-from pyramid.view import view_config
 
 from substanced.sdi import mgmt_view
 from substanced.form import FormView
 from substanced.interfaces import IFolder
 
-from .resources import Document
 from .resources import DocumentSchema
 
 #
