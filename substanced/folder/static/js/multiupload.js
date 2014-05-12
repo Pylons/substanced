5a3011840fd6377be8bd4019d390971969d68faf
Balazs Ree <ree@greenfinity.hu>
2014-05-12 19:32:05 +0200

    Give up the idea of individual upload + cancel buttons. First, I am not sure if we want the individuality. Second, it will not work well, or at all, with multifile requests.

--- substanced/folder/static/js/multiupload.js
+++ substanced/folder/static/js/multiupload.js
@@ -6,7 +6,7 @@
 
     function flash(alertType, diff) {
         // Alert type is either 'success' or 'error'.
-        // Diff is (sensibly) positive.
+        // Diff is a positive value.
         //
         // is there already a flash for this alert type?
         var flashBox = $('#messages').find('.alert-fileupload-' + alertType).last();
@@ -39,20 +39,8 @@
         flashBox.data().increment(diff);
     }
 
-    function wrapMethod(button, name) {
-        // patches the method to call the global _and_ the individual
-        // button's corresponding method. Used if called from global button.
-        var data = button.data();
-        var old = data[name];
-        data[name] = function() {
-            old.apply(this, arguments);
-            var singleButton = data.uploadButton;
-            singleButton.data()[name].apply(singleButton, arguments);
-        };
-    }
-
     var url = './@@upload-submit',
-        uploadButton = $('<button class="upload-button" />')
+        UploadButton = $('<button class="upload-button" />')
             .addClass('btn btn-primary')
             .prop('disabled', true)
             .on('click', function () {
@@ -67,10 +55,9 @@
                 });
             })
             .data({
-                clone: function() {
-                    // create a fresh new clone of the button
-                    // also call initialState
-                    var self = uploadButton.clone(true);
+                create: function() {
+                    // create and return a new button
+                    var self = UploadButton.clone(true);
                     var data = self.data();
                     data.self = self;
                     // set initial state
@@ -82,22 +69,46 @@
                     this.self
                         .text('Upload')
                         .prop('disabled', !!(this.files || []).error);
+                    this.handlers = {};
+                    this.handlers.submit = [];
                 },
                 uploadState: function() {
                     this.self
                         .off('click')
                         .text('Abort');
+                    // add a promise for us
+                    // to signal when finished
+                    this.finished = $.Deferred();
                 },
                 finishedState: function() {
                     this.self.remove();
+                    this.finished.resolve();
+                },
+                onSubmit: function(handler) {
+                    this.handlers.submit.push(handler);
+                },
+                submit: function() {
+                    // submit all sets of files
+                    var all = [];
+                    $.each(this.handlers.submit, function (index, handler) {
+                        all.push(handler());
+                    });
+                    // make an 'all' promise from the individual promises
+                    return $.when.apply(null, all);
                 }
             });
     $('#fileupload').fileupload({
         url: url,
         dataType: 'json',
         autoUpload: false,
+        // XXX These settings do not make sense generically,
+        // but they can be important in specific use cases.
+        // For this reason, they should be settable on the folder.
+        // TODO implement this once.
+        //
         //acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i,
         //maxFileSize: 5000000, // 5 MB
+        //
         // Enable image resizing, except for Android and Opera,
         // which actually support image resizing, but fail to
         // send Blob objects via XHR requests.
@@ -113,46 +124,21 @@
         limitMultiFileUploadSize: 5 * 1000 * 1000    // 5MB
     }).on('fileuploadadd', function (e, data) {
         data.context = $('<div/>').appendTo('#files');
-        // add a global upload button
-        // is there already one?
+        // add a global upload button, if it does not exist yet
         var button = $('#fileupload-wrapper').find('.upload-button');
         if (button.length === 0) {
-            // add a new one
-            button = uploadButton.data().clone()
-                .data({
-                    // make this into a global button
-                    // dataItems aggregates all individual upload instances
-                    dataItems: [],
-                    submit: function() {
-                        var all = [];
-                        $.each(this.dataItems, function (index, dataItem) {
-                            if (!dataItem.done) {
-                                all.push(dataItem.submit());
-                            }
-                        });
-                        return $.when.apply(null, all);
-                    }
-                })
+            button = UploadButton.data().create()
                 .appendTo('#fileupload-wrapper');
-            // wrap state changes to also act on individual buttons
-            //wrapMethod(button, 'uploadState');
-            //wrapMethod(button, 'finishedState');
         }
-        var globalData = button.data();
-        // Construct the file's upload info bar
+        // Construct the upload info bar for all the files
         $.each(data.files, function (index, file) {
             $('<p/>')
                 .append($('<span/>').text(file.name))
                 .append('<br>')
-                // Add individual upload button
-                .append(uploadButton.data().clone().data(data))
                 .appendTo(data.context);
-            // store individual buttons, so global actions
-            // can change their state appropriately.
-            data.uploadButton = uploadButton;
-            // Add the files to the global button
-            globalData.dataItems = globalData.dataItems.concat(data);
         });
+        // register the submit function on the button
+        button.data().onSubmit($.proxy(data.submit, data));
         // reset progress
         $('#progress .progress-bar').css('width', 0);
     }).on('fileuploadprocessalways', function (e, data) {
@@ -169,27 +155,27 @@
                 .append('<br>')
                 .append($('<span class="text-danger"/>').text(file.error));
         }
-        if (index + 1 === data.files.length) {
-            // XXX
-            data.context.find('button')
-                .text('Upload')
-                .prop('disabled', !!data.files.error);
-        }
+        //if (index + 1 === data.files.length) {
+        //    // XXX
+        //    data.context.find('button')
+        //        .text('Upload')
+        //        .prop('disabled', !!data.files.error);
+        //}
     }).on('fileuploadprogressall', function (e, data) {
         var progress = parseInt(data.loaded / data.total * 100, 10);
         $('#progress .progress-bar').css('width', progress + '%');
     }).on('fileuploaddone', function (e, data) {
         // status for the user
         flash('success', data.result.files.length);
-        $.each(data.result.files, function (index, file) {
-            console.log('DONE file:', index, file);
-        });
     }).on('fileuploadfail', function (e, data) {
         // status for the user
         flash('error', data.result.files.length);
-        $.each(data.result.files, function (index, file) {
-            console.log('ERROR file:', index, file);
-        });
+        //$.each(data.result.files, function (index, file) {
+        //    console.log('ERROR file:', index, file);
+        //});
+    }).on('fileuploadalways', function (e, data) {
+        // Satisfy my finished promise.
+        data.finished.resolve();
     }).prop('disabled', !$.support.fileInput)
         .parent().addClass($.support.fileInput ? undefined : 'disabled');
 })(jQuery);
