3eae199ea079101d1fe7100746dddd171c20f1f0
Balazs Ree <ree@greenfinity.hu>
2014-06-22 19:59:40 +0200

    Mark generated css as non-diff in .gitattributes. Also, generate a banner.

--- substanced/folder/generate/Gruntfile.js
+++ substanced/folder/generate/Gruntfile.js
@@ -7,6 +7,9 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         sprite: {
+            'options': {
+                banner: '/*! substanced.folder generated resource */\n',
+            },
             'default': {
                 src: 'node_modules/free-file-icons/48px/*.png',
                 destImg: '../static/css/images/mimetype-icons.png',
@@ -18,11 +21,26 @@ module.exports = function(grunt) {
                 },
             },
         },
+        usebanner: {
+            'default': {
+                options: {
+                    banner: '/*\n' +
+                            ' * Resource generated automatically by substanced.folder.\n' +
+                            ' * DO NOT EDIT! Regenerate with:\n' +
+                            ' *\n' +
+                            ' * $ cd substanced/folder/generate; npm install\n' +
+                            ' */\n',
+                },
+                files: {
+                    src: '<%= sprite.default.destCSS %>',
+                },
+            },
+        },
     });
 
     require('load-grunt-tasks')(grunt);
 
-    grunt.registerTask('install', ['sprite']);
+    grunt.registerTask('install', ['sprite', 'usebanner']);
 
     grunt.registerTask('default', 'install');
 
